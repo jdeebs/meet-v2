c14a3ba3e9cc80fd771ec82009510def15c3bd98
Jacob DiBiase <jakesback93@hotmail.com>
2024-08-17 13:32:27 -0700

    Copy all files from meet, excluding auth-server directory

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import CitySearch from "./components/CitySearch/CitySearch";
+import EventList from "./components/EventList/EventList";
+import NumberOfEvents from "./components/NumberOfEvents/NumberOfEvents";
+import CityEventsChart from "./components/CityEventsChart/CityEventsChart"
+import { InfoAlert, ErrorAlert, WarningAlert } from "./components/Alert/Alert";
+import { getEvents, extractLocations } from "./api";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [allLocations, setAllLocations] = useState([]);
+  const [events, setEvents] = useState([]);
+  // State for current number of events to be displayed
+  const [numberOfEvents, setNumberOfEvents] = useState(32);
+  // State for selected city to filter events
+  const [selectedCity, setSelectedCity] = useState("");
+  const [infoAlert, setInfoAlert] = useState("");
+  const [errorAlert, setErrorAlert] = useState("");
+  const [warningAlert, setWarningAlert] = useState("");
+
+  // Hook to fetch data whenever numberOfEvents or selectedCity changes
+  useEffect(() => {
+    const fetchData = async () => {
+      const allEvents = await getEvents();
+      const locations = extractLocations(allEvents);
+      setAllLocations(locations);
+      const filteredEvents = await getEvents(selectedCity);
+      setEvents(filteredEvents.slice(0, numberOfEvents));
+    };
+    // Display warning message if user is offline
+    if (!navigator.onLine) {
+      setWarningAlert("You are currently offline. New events will not update until a connection is established.");
+    } else {
+      setWarningAlert("");
+    }
+    fetchData();
+  }, [numberOfEvents, selectedCity]);
+
+  // Handler function to update the selectedCity state
+  const handleCitySelection = async (city) => {
+    setSelectedCity(city);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="alerts-container">
+        {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
+        {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+        {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
+      </div>
+      <h1 id="title">Meet App</h1>
+      <CitySearch
+        onCitySelect={handleCitySelection}
+        setInfoAlert={setInfoAlert}
+      />
+      <NumberOfEvents
+        setNumberOfEvents={setNumberOfEvents}
+        setErrorAlert={setErrorAlert}
+      />
+      <CityEventsChart allLocations={allLocations} events={events} />
+      <EventList events={events} />
     </div>
   );
-}
+};
 
 export default App;
